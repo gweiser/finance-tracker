2dc1ea59b495da15f9c54f02093fcb89a718f47e
gregolator <gweiser54@icloud.com>
2023-12-07 22:15:44 +0100

    .

--- website/__init__.py
+++ website/__init__.py
@@ -6,7 +6,9 @@ def create_app():
     app.config['SECRET_KEY'] = 'foo'
 
     from .views import views
+    from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/auth')
     
-    return app
+    return app 
--- website/views.py
+++ website/views.py
@@ -5,3 +5,9 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return '<h1>Foo</h1>'
+
+
+@views.route('/add')
+def add():
+    pass
+
