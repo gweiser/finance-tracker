ca2f221cece10a62d74584df8b4cb259a65adfcc
gregolator <gweiser54@icloud.com>
2023-12-13 19:57:56 +0100

    Continued setting up database, finished for now

--- website/schema.sql
+++ website/schema.sql
@@ -1,5 +1,19 @@
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-    id 
+    id int NOT NULL PRIMARY KEY,
+    username varchar(250) NOT NULL,
+    hashed_pwd varchar(250) NOT NULL
+    );
+
+
+DROP TABLE IF EXISTS expenses;
+
+CREATE TABLE expenses (
+    id int NOT NULL PRIMARY KEY,
+    amount int NOT NULL,
+    note varchar(250) NOT NULL,
+    expense_location varchar(250) NOT NULL,
+    expense_date date NOT NULL,
+    user_id int NOT NULL FOREIGN KEY REFERENCES users(id)
 );
