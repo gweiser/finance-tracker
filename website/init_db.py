f8cdcb664398875c8751e8c99d63c273cb230b32
gregolator <gweiser54@icloud.com>
2023-12-14 15:46:36 +0100

    Fully migrated DB, couldn't get schema into seperate file

--- website/init_db.py
+++ website/init_db.py
@@ -3,10 +3,32 @@ import sqlite3
 connection = sqlite3.connect('database.db')
 
 
-with open('schema.sql') as f:
-    connection.executescript(f.read())
+# with open("schema.sql") as f:
+#     connection.executescript(f.read())
 
-cur = connection.cursor()
+connection.executescript("""
+    DROP TABLE IF EXISTS users;
 
-connection.commit()
+    CREATE TABLE users (
+        id int NOT NULL PRIMARY KEY,
+        username varchar(250) NOT NULL,
+        hashed_pwd varchar(250) NOT NULL
+        );
+
+
+    DROP TABLE IF EXISTS expenses;
+
+    CREATE TABLE expenses (
+        id int NOT NULL PRIMARY KEY,
+        amount int NOT NULL,
+        note varchar(250) NOT NULL,
+        expense_location varchar(250) NOT NULL,
+        expense_date date NOT NULL,
+        user_id int NOT NULL,                         
+        FOREIGN KEY(user_id) REFERENCES users(id)
+    );      
+                   """)
+
+
+connection.commit() 
 connection.close()
